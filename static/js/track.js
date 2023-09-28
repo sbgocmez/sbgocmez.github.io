7690bb9347d89fcb506fa1ec346ed0853d866fbb
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-28 16:42:03 +0300

    if-else sacmalik

--- static/js/track.js
+++ static/js/track.js
@@ -29,9 +29,11 @@ async function trackApple() {
 
 async function trackEverything() {
 
-    const returned_data = trackApple()
+    //const returned_data = trackApple()
 
-    if (returned_data == null) {
+    var is_apple = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
+
+    if (!is_apple) {
     var platform = navigator.userAgentData.platform
     var device = navigator.userAgentData.mobile === true ? "mobile" : "desktop";
     var language = navigator.language
@@ -65,24 +67,61 @@ async function trackEverything() {
 
     // constructed data for apple products
     else {
-        const data = returned_data
+
+        var is_Mac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+        var is_iPhone = navigator.platform == "iPhone";
+
+        var language = navigator.language
+
+        if (is_Mac) {
+            const apple_data = {
+            "device": "desktop",
+            "platform": "MacOS",
+            "language": language
+            }
         fetch('https://fastapi-busra-736f01987e01.herokuapp.com/clients', {
         method: 'POST',
-        body: JSON.stringify(data),
+        body: JSON.stringify(apple_data),
         headers: {
             'Content-Type': 'application/json'
         }
-    })
-    .then(response => {
-        // Handle the response, e.g., log success or handle errors
-        console.log('Tracking data sent successfully');
-        console.log(data);
-        console.log(body)
-    })
-    .catch(error => {
-        console.error('Error sending tracking data:', error);
-    });
-    }
+        })
+        .then(response => {
+            // Handle the response, e.g., log success or handle errors
+            console.log('Tracking data sent successfully');
+            console.log(apple_data);
+            console.log(body)
+        })
+        .catch(error => {
+            console.error('Error sending tracking data:', error);
+        });
+        }
+        else if (is_iPhone)
+        {
+            const iphone_data = {
+            "device": "mobile",
+            "platform": "iOS",
+            "language": language
+            }
+            fetch('https://fastapi-busra-736f01987e01.herokuapp.com/clients', {
+            method: 'POST',
+            body: JSON.stringify(iphone_data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            })
+            .then(response => {
+                // Handle the response, e.g., log success or handle errors
+                console.log('Tracking data sent successfully');
+                console.log(iphone_data);
+                console.log(body)
+            })
+            .catch(error => {
+                console.error('Error sending tracking data:', error);
+            });
+        }
+
+}
     
 }
 
