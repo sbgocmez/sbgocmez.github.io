579518ba6e06ae4146156eb113c3c0ec5a171875
Sakine Busra Gocmez <62428632+sbgocmez@users.noreply.github.com>
2023-09-28 15:21:46 +0300

    apple check added

--- static/js/track.js
+++ static/js/track.js
@@ -1,10 +1,42 @@
 
 
+async function trackApple() {
+    var is_apple = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
+    var is_iOS = /(iPhone|iPod|iPad)/i.test(navigator.platform);
+
+    var is_Mac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+    var is_iPhone = navigator.platform == "iPhone";
+
+    var language = navigator.language
+
+    if (is_Mac && is_apple) {
+    const data = {
+        "device": "desktop",
+        "platform": "MacOS",
+        "language": language
+    }
+    return data
+    } else if (is_iPhone && is_iOS){
+        const data = {
+        "device": "mobile",
+        "platform": "iOS",
+        "language": language
+    }
+    return data
+    }
+    return null
+}
+
 async function trackEverything() {
+
+    var returned_data = trackApple()
+
+    if (returned_data == null) {
     var platform = navigator.userAgentData.platform
     var device = navigator.userAgentData.mobile === true ? "mobile" : "desktop";
     var language = navigator.language
     
+    // construct the data
     const data = {
         "device": platform,
         "platform": device,
@@ -12,7 +44,12 @@ async function trackEverything() {
     }
     console.log("my dataa")
     console.log(data)
+    }
 
+    // constructed data for apple products
+    else {
+        var data = returned_data
+    }
     fetch('https://fastapi-busra-736f01987e01.herokuapp.com/clients', {
         method: 'POST',
         body: JSON.stringify(data),
